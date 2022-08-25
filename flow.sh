b7d716adecb37aebf5e7acea31cda4c2caa2161c
Tentikurisu <charlieauletta@gmail.com>
2022-08-25 12:01:56 +0100

    added some comments to explain the new code

--- flow.sh
+++ flow.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cmnt='userinput'
+#creating varible to hold the user input for the comment
 
 # stage changes
 git add flow.sh
@@ -7,7 +8,7 @@ git add flow.sh
 # commit changes
 echo -n "please enter a comment: "
 read cmnt
-
+#displays a message asking the user to give a comment and setting the comment
 git commit -m "$cmnt"
 
 # push changes
